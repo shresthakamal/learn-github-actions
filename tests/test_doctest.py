646a08288cc05bf46913e05f1e7070573dadbe73
Kamal Shrestha <kshrestha1@godaddy.com>
2026-04-27 10:49:31 +0545

    more tests

---
+++ tests/test_doctest.py
@@ -0,0 +1,6 @@
+def something():
+    """a doctest in a docstring
+    >>> something()
+    42
+    """
+    return 42
---
+++ tests/test_expectation.py
@@ -0,0 +1,16 @@
+# content of test_expectation.py
+import pytest
+
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
+@pytest.mark.parametrize("n,expected", [(1, 2), (3, 4)])
+class TestClass:
+    def test_simple_case(self, n, expected):
+        assert n + 1 == expected
+
+    def test_weird_simple_case(self, n, expected):
+        assert (n * 1) + 1 == expected
